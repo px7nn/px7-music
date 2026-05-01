cd6097666a1fa19963915d2cef849309beeadc89
px7nn <px7nnn@gmail.com>
2026-05-01 16:02:09 +0530

    fix MPV auto-play back feature

--- px7_music/player/player.py
+++ px7_music/player/player.py
@@ -6,8 +6,13 @@ class PlayerMPV(Player):
         self._mpv = mpv
         self.player = mpv.MPV(video=False, log_handler=None, loglevel="error")
         self._end_callback = None
+        
+        @self.player.event_callback(mpv.MpvEventID.END_FILE)
+        def on_end(event):
+            if self._end_callback:
+                self._end_callback()
 
-        self.player.event_callback(self._on_end)
+        self._on_end_handler = on_end
 
     def _on_end(self, event):
         if event.event_id == self._mpv.MPV_EVENT_END_FILE:
